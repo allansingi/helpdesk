c6b47cdfb7b0e227506a136256f4bced342073a2
allansingi <allansingi@gmail.com>
2023-03-30 17:11:51 +0100

    end-point findById para Tecnico

---
+++ src/main/java/com/allanborges/helpdesk/services/TecnicoService.java
@@ -0,0 +1,23 @@
+package com.allanborges.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.allanborges.helpdesk.domain.Tecnico;
+import com.allanborges.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+
+	@Autowired
+	private TecnicoRepository repository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElse(null);
+		
+	}
+	
+}
