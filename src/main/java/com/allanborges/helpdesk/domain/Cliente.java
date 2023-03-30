28475f7bd31390c49fa641fff62dd1bfc6b75b18
allansingi <allansingi@gmail.com>
2023-03-30 17:11:18 +0100

    protegendo aplicação contra serialização

--- src/main/java/com/allanborges/helpdesk/domain/Cliente.java
+++ src/main/java/com/allanborges/helpdesk/domain/Cliente.java
@@ -7,11 +7,13 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.allanborges.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
