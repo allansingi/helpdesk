f4a039ff8ebcfe41dafdbb977cd4c035d8a6c883
allansingi <allansingi@gmail.com>
2023-03-31 09:40:52 +0100

    Padrao DTO para Tecnico

---
+++ src/main/java/com/allanborges/helpdesk/domain/dtos/TecnicoDTO.java
@@ -0,0 +1,84 @@
+package com.allanborges.helpdesk.domain.dtos;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.allanborges.helpdesk.domain.Tecnico;
+import com.allanborges.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class TecnicoDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	protected Integer id;
+	protected String nome;
+	protected String nif;
+	protected String email;
+	protected String senha;
+	protected Set<Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	protected LocalDate dataCriacao = LocalDate.now();
+
+	public TecnicoDTO() {
+		super();
+	}
+
+	public TecnicoDTO(Tecnico obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.nif = obj.getNif();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNif() {
+		return nif;
+	}
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+	public void addPerfil(Perfil perfil) {
+		this.perfis.add(perfil.getCodigo());
+	}
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+}
