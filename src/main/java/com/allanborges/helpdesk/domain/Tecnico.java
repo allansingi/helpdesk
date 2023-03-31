b1659f00dc758e70d40821b6f7729e60616a2517
allansingi <allansingi@gmail.com>
2023-03-31 11:18:16 +0100

    End-point create para tecnico

--- src/main/java/com/allanborges/helpdesk/domain/Tecnico.java
+++ src/main/java/com/allanborges/helpdesk/domain/Tecnico.java
@@ -2,10 +2,12 @@ package com.allanborges.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.allanborges.helpdesk.domain.dtos.TecnicoDTO;
 import com.allanborges.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -27,6 +29,17 @@ public class Tecnico extends Pessoa {
 		addPerfil(Perfil.CLIENTE);
 	}
 
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.nif = obj.getNif();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+	
 	public List<Chamado> getChamados() {
 		return chamados;
 	}
