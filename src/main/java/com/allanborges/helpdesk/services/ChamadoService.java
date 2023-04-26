65c18278355e2ed02236a145b36bf82ffabf027a
allansingi <allansingi@gmail.com>
2023-04-26 14:42:04 +0100

    update para chamado

--- src/main/java/com/allanborges/helpdesk/services/ChamadoService.java
+++ src/main/java/com/allanborges/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.allanborges.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,6 +42,14 @@ public class ChamadoService {
 	public Chamado create(@Valid ChamadoDTO objDTO) {
 		return repository.save(newChamado(objDTO));
 	}
+
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
+		
+	}
 	
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
@@ -49,6 +58,10 @@ public class ChamadoService {
 		Chamado chamado = new Chamado();
 		if(obj.getId() != null) {
 			chamado.setId(obj.getId());
+		}
+		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
 		}
 		
 		chamado.setTecnico(tecnico);
